28e5afadab5380f954383b519a818d34a81aa145
unknown <syoung0226@gmail.com>
2015-07-25 16:27:37 +0900

    startButton click 하면  ExerciseActivity.java 나옴

---
+++ app/src/main/java/com/example/syoung/fitsy/main/ExerciseActivity.java
@@ -0,0 +1,17 @@
+package com.example.syoung.fitsy.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.syoung.fitsy.R;
+
+/**
+ * Created by syoung on 2015-07-25.
+ */
+public class ExerciseActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fitsy_main_exercise);
+    }
+}
--- app/src/main/java/com/example/syoung/fitsy/main/MainFragment.java
+++ app/src/main/java/com/example/syoung/fitsy/main/MainFragment.java
@@ -1,38 +1,27 @@
 package com.example.syoung.fitsy.main;
 
-import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
-import android.widget.ProgressBar;
-import android.widget.Toast;
+import android.widget.ImageButton;
 
 import com.example.syoung.fitsy.R;
-import com.example.syoung.fitsy.main.adapter.BluetoothListAdapter;
-import com.example.syoung.fitsy.main.bluetooth.ConnectThread;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.OnItemClick;
 
 public class MainFragment extends Fragment {
 
     private View rootView;
     private static MainFragment instance;
+
+    @Bind(R.id.startBtn) ImageButton startBtn;
 
     public MainFragment() {
 
@@ -54,5 +43,11 @@ public class MainFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
+    }
+
+    @OnClick(R.id.startBtn)
+    public void exerciseStart() {
+        Intent exerciseIntent = new Intent(this.getActivity(), ExerciseActivity.class);
+        startActivity(exerciseIntent);
     }
 }
