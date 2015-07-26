33de94674c223c1b838bb4952f91d21c5a7ca36c
Lee Sang Hooon <hun6728@naver.com>
2015-07-26 16:20:43 +0900

    Scroll View + Layout 개략적으로 작성

--- app/src/main/java/com/example/syoung/fitsy/myinfo/MyInformationFragment.java
+++ app/src/main/java/com/example/syoung/fitsy/myinfo/MyInformationFragment.java
@@ -1,10 +1,14 @@
 package com.example.syoung.fitsy.myinfo;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.syoung.fitsy.R;
 
@@ -13,11 +17,12 @@ import com.example.syoung.fitsy.R;
  */
 public class MyInformationFragment extends Fragment {
     private View rootView;
-
+    private ImageView img;
+    TextView tv1,tv2,tv3;
+    private Button btn1,btn2;
     private static MyInformationFragment instance;
 
     public MyInformationFragment() {
-
     }
 
     public static MyInformationFragment getInstance() {
@@ -30,6 +35,31 @@ public class MyInformationFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         rootView = inflater.inflate(R.layout.fragment_my_information, container, false);
+        img = (ImageView)rootView.findViewById(R.id.imageView);
+        tv1=(TextView)rootView.findViewById(R.id.text2);
+        tv2=(TextView)rootView.findViewById(R.id.text3);
+        tv3=(TextView)rootView.findViewById(R.id.text4);
+        tv1.setText("이름 : 이상훈");
+        tv2.setText("키: 180cm");
+        tv3.setText("몸무게: 71kg");
         return rootView;
+    }
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.button1:
+                btn1.setBackgroundColor(Color.BLUE);
+                btn1.setTextColor(Color.WHITE);
+                btn2.setBackgroundColor(Color.WHITE);
+                btn2.setTextColor(Color.BLACK);
+                break;
+            case R.id.button2:
+                btn1.setBackgroundColor(Color.WHITE);
+                btn1.setTextColor(Color.BLACK);
+                btn2.setBackgroundColor(Color.BLUE);
+                btn2.setTextColor(Color.WHITE);
+                break;
+            default:
+                break;
+        }
     }
 }
