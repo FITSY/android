ff033bb8917fdf182dfd427483b267b1ae13e780
unknown <syoung0226@gmail.com>
2015-08-07 00:41:14 +0900

    finish exercise

--- app/src/main/java/com/example/syoung/fitsy/main/ExerciseActivity.java
+++ app/src/main/java/com/example/syoung/fitsy/main/ExerciseActivity.java
@@ -25,7 +25,6 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.OnItemClick;
 
-//TODO : 운동중 운동 된거는 이미지 파란색으로 하고 선택하면 이미 한 운동입니다 토스트 띄우기
 public class ExerciseActivity extends Activity {
 
     @Bind(R.id.exerciseFinishBtn) ImageButton exerciseFinishBtn;
@@ -46,15 +45,13 @@ public class ExerciseActivity extends Activity {
     private List<NowCourse> nowExerciseCourseItemList;
     private NowCourse nowExercise;
     private int exerciseType = 1;
+
     private int mainTime = 0;
     private int min = 0;
     private int sec = 0;
     private int boundMin = 0;
     private Timer mTimer;
     private int nowExerciseIndex = 0;
-
-    //TODO : 운동을 한 정보는 NowCourse check 에 했는지 안 했는지에 대한 정보와 NowCourse에 result에 무산소 운동 횟수와 유산소운동 시간을 입력
-    //TODO : 한 운동은 list 에서 색칠되고 클릭 안되게 하기(처음 화면 킬 때NowCourse가 check인 것들 setting 해주기)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,49 +97,65 @@ public class ExerciseActivity extends Activity {
     }
 
     private void setViewComponent() {
-        int i = 0;
+        int index = 0;
         for (NowCourse nowCourse : nowExerciseCourseItemList) {
+            if(nowCourse.isCheck()){
+                Toast.makeText(this, "이미 한 운동입니다.", Toast.LENGTH_SHORT).show();
+                intentNFCReadActivity();
+                return;
+            }
             if (nowCourse.getUserCourse().getOdid() != null && nowCourse.getUserCourse().getOdid().equals(exerciseData.getTagId())) {
                 nowExercise = nowCourse;
-                nowExerciseIndex = i;
+                nowExerciseIndex = index;
                 exerciseName.setText(nowCourse.getUserCourse().getEname().replace("_", " ").toUpperCase());
                 if (nowCourse.getUserCourse().getOtype() == 1) {
-                    //무산소
-                    exerciseType = 1;
-                    optionOneKey.setText("weight");
-                    optionTwoKey.setText("count");
-                    optionOneValue.setText(String.valueOf(nowCourse.getUserCourse().getOoption1()) + "kg");
-                    optionTwoValue.setText(String.valueOf(nowCourse.getUserCourse().getOoption2()) + "count");
-                    nowNumber1.setText(String.valueOf(0));
-                    numDivider.setText("/");
-                    nowNumber2.setText(String.valueOf(nowCourse.getUserCourse().getOoption2()));
+                    initComponentExerciseTypeOne(nowCourse);//무산소
                 } else {
-                    //유산소
-                    exerciseType = 2;
-                    optionOneKey.setText("speed");
-                    optionTwoKey.setText("running Time");
-                    optionOneValue.setText(String.valueOf(nowCourse.getUserCourse().getOoption1()) + " km/h");
-                    boundMin = nowCourse.getUserCourse().getOoption2();
-                    optionTwoValue.setText(String.valueOf(boundMin) + " min");
-                    nowNumber1.setText("0");
-                    numDivider.setText(":");
-                    nowNumber2.setText("0");
+                    initComponentExerciseTypeTwo(nowCourse);//유산소
                 }
                 return;
             }
-            i++;
+            index++;
         }
         Toast.makeText(this, "운동코스에 없는 NFC ID 입니다.", Toast.LENGTH_SHORT).show();
         intentNFCReadActivity();
     }
 
+    @OnClick(R.id.exerciseFinishBtn)
+    public void finishExercise() {
+        //TODO : server 에 최종 운동 결과 전송
+        finish();
+    }
+
+    @OnClick(R.id.exercise_method_btn)
+    public void showExerciseMethod() {
+        //TODO : 유산소 일 때 timer 일시정지, 무산소 일 때 횟수 counting 일시 정지
+        Intent exerciseIntent = new Intent(this, ExerciseMethodActivity.class);
+        exerciseIntent.putExtra("nowExercise", (Serializable) nowExercise);
+        startActivity(exerciseIntent);
+    }
+
+    @OnItemClick(R.id.main_now_exercise_course_list)
+    void OnItemClicked(int position) {
+        if(nowExerciseCourseItemList.get(position).isCheck()){
+            Toast.makeText(this, "이미 한 운동입니다.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //TODO : 중간에 이동 할 때 현재 유산소 운동에 대한 정보, 무산소 운도 일 때 결과 정보 저장
+        if(exerciseType == 1){
+
+        }else if(exerciseType == 2){
+
+        }
+
+        intentNFCReadActivity();
+    }
+
     private void intentNFCReadActivity() {
         if(exerciseType == 2){
-            //TODO : 운동 했다고 하기
-            nowExerciseCourseItemList.get(nowExerciseIndex).setImageId(this.getResources().getIdentifier("camera", "drawable", this.getPackageName()));
-            nowExerciseCourseItemList.get(nowExerciseIndex).setCheck(true);
-            nowExerciseCourseItemList.get(nowExerciseIndex).setResult(min);
-            mTimer.cancel();
+            exerciseTypeTwoFinish();
+        }else if(exerciseType == 1){
+            //TODO : 무산소 운동 결과 setting
         }
         Intent exerciseIntent = new Intent(this, NFCReadActivity.class);
         exerciseIntent.putExtra("nowExerciseCourseList", (Serializable) nowExerciseCourseItemList);
@@ -150,9 +163,11 @@ public class ExerciseActivity extends Activity {
         finish();
     }
 
-    @Override
-    public void onBackPressed() {
-        //super.onBackPressed();
+    private void exerciseTypeTwoFinish() {
+        nowExerciseCourseItemList.get(nowExerciseIndex).setImageId(this.getResources().getIdentifier("exercise_one_finish", "drawable", this.getPackageName()));
+        nowExerciseCourseItemList.get(nowExerciseIndex).setCheck(true);
+        nowExerciseCourseItemList.get(nowExerciseIndex).setResult(min);
+        mTimer.cancel();
     }
 
     private void setNowExerciseCourseHorizontalListView() {
@@ -161,20 +176,31 @@ public class ExerciseActivity extends Activity {
         nowExerciseCourseHorizontalListView.setAdapter(nowExerciseCourseListAdapter);
     }
 
-    @OnClick(R.id.exerciseFinishBtn)
-    public void finishExercise() {
-        finish();
+    private void initComponentExerciseTypeTwo(NowCourse nowCourse) {
+        exerciseType = 2;
+        optionOneKey.setText("speed");
+        optionTwoKey.setText("running Time");
+        optionOneValue.setText(String.valueOf(nowCourse.getUserCourse().getOoption1()) + " km/h");
+        boundMin = nowCourse.getUserCourse().getOoption2();
+        optionTwoValue.setText(String.valueOf(boundMin) + " min");
+        nowNumber1.setText("0");
+        numDivider.setText(":");
+        nowNumber2.setText("0");
     }
 
-    @OnClick(R.id.exercise_method_btn)
-    public void showExerciseMethod() {
-        Intent exerciseIntent = new Intent(this, ExerciseMethodActivity.class);
-        exerciseIntent.putExtra("nowExercise", (Serializable) nowExercise);
-        startActivity(exerciseIntent);
+    private void initComponentExerciseTypeOne(NowCourse nowCourse) {
+        exerciseType = 1;
+        optionOneKey.setText("weight");
+        optionTwoKey.setText("count");
+        optionOneValue.setText(String.valueOf(nowCourse.getUserCourse().getOoption1()) + "kg");
+        optionTwoValue.setText(String.valueOf(nowCourse.getUserCourse().getOoption2()) + "count");
+        nowNumber1.setText(String.valueOf(0));
+        numDivider.setText("/");
+        nowNumber2.setText(String.valueOf(nowCourse.getUserCourse().getOoption2()));
     }
 
-    @OnItemClick(R.id.main_now_exercise_course_list)
-    void OnItemClicked(int position) {
-        intentNFCReadActivity();
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
     }
 }
